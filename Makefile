cbc038a3c4b300cfc68f4bfbb8dc43f60c46b255
Ryan Piwowarski <rpiwowarski@aetheros.com>
2023-04-13 14:25:37 -0500

    initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+
+-include config.mk
+
+CXXFLAGS += -MMD -MP -Ilib
+LIBS += -lsdk_m2m -lsdk_aos -lm2mgen -lxsd_mtrsvc -lxsd -lcoap -ldtls -lcommon -lappfw
+
+EXE = aos_wc_app
+
+SRCS = $(wildcard *.cpp)
+OBJS = $(SRCS:.cpp=.o)
+DEPS = $(SRCS:.cpp=.d)
+
+all: $(EXE)
+
+$(EXE): $(OBJS)
+	$(CXX) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+-include $(DEPS)
+
+clean:
+	rm -f $(EXE) $(OBJS) $(DEPS) *.aos
+
+.PHONY: all clean
+
---
+++ aos_waveform_app.cpp
@@ -0,0 +1,74 @@
+// vim: sw=4 expandtab
+// Copyright (c) Aetheros, Inc.  See COPYRIGHT
+#include <aos/AppMain.hpp>
+#include <aos/Log.hpp>
+#include <aos/WaveformProvider.hpp>
+#include <unistd.h>
+#include <string.h>
+#include <fstream>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char *argv[])
+{
+    aos::AppMain appMain;
+
+	logInfo( "starting" );
+
+	auto socket = aos::waveform_provider::open_socket();
+	if( socket == -1 )
+	{
+		logError( "could not open waveform provider: " << strerror( errno ) );
+		exit( -1 );
+	}
+
+	aos::waveform_provider::Packet packet;
+
+	std::ofstream ofile;
+
+	uint16_t expected_sync = 99;
+	uint16_t expected_sequence = 0;
+	uint16_t write_count = 0;
+
+	while( true )
+	{
+		auto amount = ::read( socket, &packet, sizeof(packet) );
+		if( amount < 0 )
+		{
+			logError( "could not read waveform provider" );
+			break;
+		}
+
+		if( packet.sync != expected_sync or packet.sequence != expected_sequence )
+		{
+			logWarn( "unexpected packet: " << packet.sync << " " << packet.sequence
+				<< ", expected: " << expected_sync << " " << expected_sequence );
+			ofile.close();
+			ofile.open( "output", std::ios::binary | std::ios::trunc );
+			if( not ofile )
+			{
+				logError( "could not open output file" );
+			}
+			write_count = 0;
+			expected_sync = packet.sync;
+			expected_sequence = packet.sequence;
+		}
+
+		++expected_sequence;
+
+		if( ofile and write_count < 10 )
+		{
+			for( int i=0; i<64; ++i )
+			{
+				ofile << aos::waveform_provider::voltage_scale() * packet.blocks[i].u1 << '\n';
+			}
+			++write_count;
+			if( write_count >= 10 )
+			{
+				logInfo( "wrote out samples" );
+				ofile.close();
+			}
+		}
+	}
+}
