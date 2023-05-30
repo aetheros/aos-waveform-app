d4d41172d117585929dddf2a9eecd707d72b038b
Ryan Piwowarski <rpiwowarski@aetheros.com>
2023-05-30 11:02:59 -0500

    fix error flag check

--- aos_waveform_app.cpp
+++ aos_waveform_app.cpp
@@ -16,53 +16,83 @@ int main(int argc, char *argv[])
 
 	logInfo( "starting" );
 
-	auto socket = aos::waveform_provider::open_socket( 0 );
-	if( socket == -1 )
-	{
-		logError( "could not open waveform provider: " << strerror( errno ) );
-		exit( -1 );
-	}
-
-	aos::waveform_provider::Packet packet;
-
-	std::ofstream ofile;
-
+	int socket = -1;
 	uint16_t expected_sync = 0;
 	uint16_t expected_sequence = 0;
 	uint16_t write_count = 0;
 
+	aos::waveform_provider::Packet packet;
+	auto network = aos::waveform_provider::Network::Unknown;
+
+	bool start_new_file = true;
+	std::ofstream ofile;
+
 	while( true )
 	{
+		if( socket == -1 )
+		{
+			socket = aos::waveform_provider::open_socket( 0 );
+			if( socket == -1 )
+			{
+				logError( "could not open waveform provider: " << strerror( errno ) );
+				std::this_thread::sleep_for( std::chrono::seconds( 30 ) );
+				continue;
+			}
+		}
+
 		ssize_t amount = ::read( socket, &packet, sizeof(packet) );
 		if( amount <= 0 )
 		{
+			::close( socket );
+			socket = -1;
 			logError( "could not read waveform provider" );
-			exit( 1 );
+			std::this_thread::sleep_for( std::chrono::seconds( 30 ) );
+			continue;
+		}
+
+		if( network == aos::waveform_provider::Network::Unknown )
+		{
+			network = packet.network;
+			switch( network )
+			{
+			case aos::waveform_provider::Network::OnePhaseOneElement:
+				logInfo( "u1 and i1 valid" );
+				break;
+			case aos::waveform_provider::Network::OnePhaseTwoElement:
+				logInfo( "u2-u3 and i2-i3 valid" );
+				break;
+			case aos::waveform_provider::Network::ThreePhase:
+				logInfo( "u1-u3 and i1-i3 valid" );
+				break;
+			default:
+				logError( "unexpected network configuration" );
+			}
 		}
 
 		if( packet.sync != expected_sync or packet.sequence != expected_sequence )
 		{
 			logWarn( "unexpected packet: " << packet.sync << " " << packet.sequence
 				<< ", expected: " << expected_sync << " " << expected_sequence );
-			ofile.close();
 			expected_sync = packet.sync;
 			expected_sequence = packet.sequence;
+			start_new_file = true;
 		}
 
 		++expected_sequence;
 
-		if( not ofile )
+		if( start_new_file )
 		{
+			start_new_file = false;
+			write_count = 0;
+			ofile.close();
 			ofile.open( "output", std::ios::binary | std::ios::trunc );
 			if( not ofile )
 			{
 				logError( "could not open output file" );
-				exit( 1 );
 			}
-			write_count = 0;
 		}
 
-		if( ofile and write_count < 10 )
+		if( ofile.is_open() and ofile and write_count < 10 )
 		{
 			for( int i=0; i<64; ++i )
 			{
